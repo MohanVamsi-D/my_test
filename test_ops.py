7d4f52587ff2fec1256084bda4cc05b48e1582cc
MohanVamsi-D <143251904+MohanVamsi-D@users.noreply.github.com>
2023-11-24 14:04:14 +0530

    added test ops file

---
+++ test_ops.py
@@ -0,0 +1,17 @@
+from ops import *
+
+def test_add():
+<<<<<<< HEAD
+    assert add(2,3) == 1
+=======
+    assert add(2,3) == 4
+>>>>>>> 2ff562a3e996b0b6fa0b80535c96a379e47e5aa4
+
+def test_subtract():
+    assert subtract(2, 3) == -1
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+
+def test_divide():
+    assert divide(10,5) == 2
